3d9f98d26fbde2023923d45e201873f50c05215d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-01-18 20:23:31 -0500

    added Ocean to the Scene

--- MAPD724-W2023-ICE-1B/GameScene.swift
+++ MAPD724-W2023-ICE-1B/GameScene.swift
@@ -3,9 +3,17 @@ import GameplayKit
 
 class GameScene: SKScene
 {
+    // instance variables
+    var ocean: Ocean?
+    
     override func sceneDidLoad()
     {
-        print("Game Scene Loaded")
+        name = "GAME"
+        
+        // add the ocean to the Scene
+        ocean = Ocean()
+        ocean?.Reset()
+        addChild(ocean!)
     }
     
     func touchDown(atPoint pos : CGPoint)
@@ -44,6 +52,6 @@ class GameScene: SKScene
     
     override func update(_ currentTime: TimeInterval)
     {
-
+        ocean?.Update()
     }
 }
